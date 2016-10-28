ee13b058c69f4e62732f1d84bedc2d0b9fe7cc2a
Florian Gebhardt <florian.gebhardt@gmx.de>
2016-10-28 09:37:35 +0200

    Added source field

--- mainController.js
+++ mainController.js
@@ -7,6 +7,8 @@ rezeptrechner.controller("mainController", ['$scope', '$http', function($scope, 
 
 const Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(a){var c,d,e,f,g,h,i,b="",j=0;for(a=Base64._utf8_encode(a);j<a.length;)c=a.charCodeAt(j++),d=a.charCodeAt(j++),e=a.charCodeAt(j++),f=c>>2,g=(3&c)<<4|d>>4,h=(15&d)<<2|e>>6,i=63&e,isNaN(d)?h=i=64:isNaN(e)&&(i=64),b=b+this._keyStr.charAt(f)+this._keyStr.charAt(g)+this._keyStr.charAt(h)+this._keyStr.charAt(i);return b},decode:function(a){var c,d,e,f,g,h,i,b="",j=0;for(a=a.replace(/[^A-Za-z0-9\+\/\=]/g,"");j<a.length;)f=this._keyStr.indexOf(a.charAt(j++)),g=this._keyStr.indexOf(a.charAt(j++)),h=this._keyStr.indexOf(a.charAt(j++)),i=this._keyStr.indexOf(a.charAt(j++)),c=f<<2|g>>4,d=(15&g)<<4|h>>2,e=(3&h)<<6|i,b+=String.fromCharCode(c),64!=h&&(b+=String.fromCharCode(d)),64!=i&&(b+=String.fromCharCode(e));return b=Base64._utf8_decode(b)},_utf8_encode:function(a){a=a.replace(/\r\n/g,"\n");for(var b="",c=0;c<a.length;c++){var d=a.charCodeAt(c);d<128?b+=String.fromCharCode(d):d>127&&d<2048?(b+=String.fromCharCode(d>>6|192),b+=String.fromCharCode(63&d|128)):(b+=String.fromCharCode(d>>12|224),b+=String.fromCharCode(d>>6&63|128),b+=String.fromCharCode(63&d|128))}return b},_utf8_decode:function(a){for(var b="",c=0,d=0,f=0;c<a.length;)d=a.charCodeAt(c),d<128?(b+=String.fromCharCode(d),c++):d>191&&d<224?(f=a.charCodeAt(c+1),b+=String.fromCharCode((31&d)<<6|63&f),c+=2):(f=a.charCodeAt(c+1),c3=a.charCodeAt(c+2),b+=String.fromCharCode((15&d)<<12|(63&f)<<6|63&c3),c+=3);return b}};
 
+  $scope.domain = "localhost";
+
   $scope.new = {
     name : "",
     value: 1,
@@ -18,8 +20,10 @@ const Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234
   };
 
   $scope.recipe = {
-    name : "",
-    text : ""
+    name   : "",
+    text   : "",
+    source : "",
+    realSource : "http://"+$scope.domain+"/#"
   };
 
   $scope.factor = 1;
@@ -97,12 +101,13 @@ const Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234
     console.log("Share");
     if ($scope.form.foo.length > 0) {
       var dataArray = {
-        'name' : $scope.recipe.name,
-        'text' : $scope.recipe.text,
-        'data' : $scope.form.foo
+        'name'   : $scope.recipe.name,
+        'text'   : $scope.recipe.text,
+        'source' : $scope.recipe.source,
+        'data'   : $scope.form.foo
       };
       var data = encodeURL(Base64.encode(JSON.stringify(dataArray)));
-      window.open("http://"+window.location.hostname+"/#"+data);
+      window.open("http://"+$scope.domain+"/#"+data);
     } else {
       alert("Nichts zum Teilen :/");
     }
@@ -114,6 +119,12 @@ const Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234
     dataAll = JSON.parse(data.substring(0, data.length - 1));
     $scope.recipe.name = dataAll["name"];
     $scope.recipe.text = dataAll["text"];
+    if (dataAll["source"] !== "") {
+      document.getElementById("source").readOnly = true;
+      document.getElementById("source-a").setAttribute("target", "_blank");
+      $scope.recipe.source     = dataAll["source"];
+      $scope.recipe.realSource = dataAll["source"];
+    }
     for (var x of dataAll["data"]) {
       $scope.form.foo.push(x);
     }
