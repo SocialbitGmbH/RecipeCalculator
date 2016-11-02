7b81aa40d8aaa484d782d6c2ea3c1535360d0e05
Florian Gebhardt <florian.gebhardt@gmx.de>
2016-11-02 19:27:36 +0100

    Made stylechanging more flexible

--- js/resize.js
+++ js/resize.js
@@ -1,19 +1,26 @@
+function changeStyle (element, value, filter) {
+  if (filter === undefined) filter = /.+/;
+  var current = element.style.cssText;
+  element.style = current.replace(filter, "") + value;
+}
+
 function resize () {
 
 //----<<< Left container >>>----\\
 
 var height = document.getElementsByTagName("body")[0].clientHeight;
-height -= 60;
+height -= document.getElementsByClassName("bottom-container")[0].clientHeight;
 
-document.getElementById("left-container").style = "height:"+height+"px;";
+changeStyle(document.getElementById("left-container"), "height:"+height+"px;", /height:\s\d+px;/);
+changeStyle(document.getElementsByClassName("cook-container")[0], "height:"+height+"px;", /height:\s\d+px;/)
 
-height    -= document.getElementById("menu").offsetHeight + 29;
+height    -= document.getElementById("menu").offsetHeight;
 height    -= document.getElementsByClassName("th")[0].offsetHeight;
-height    -= document.getElementsByClassName("add-data")[0].offsetHeight;
+height    -= document.getElementsByClassName("add-data")[0].offsetHeight + 29;
 
 if (height < 75) height = 75;
 
-document.getElementsByClassName("recipe-container")[0].style = "height:"+height+"px;";
+changeStyle(document.getElementsByClassName("recipe-container")[0], "height: "+height+"px;", /height:\s\d+px;/);
 }
 
 window.addEventListener("resize", resize);
